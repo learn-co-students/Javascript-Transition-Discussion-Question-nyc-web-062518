e6e7c74e46f1096973a3cab3b7a92d9f163adf2f
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-08-09 10:36:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
+
+const interval = document.getElementById("interval")
+const button = document.getElementById("save_lincoln")
+const zombie = document.getElementById("foreground")
+
+function fadeOut(interval) {
+  const fadeEffect = setInterval(function () {
+    if (!zombie.style.opacity) {
+      zombie.style.opacity = 1
+      console.log("set opacity of zombie");
+    }
+    if (zombie.style.opacity > 0) {
+      zombie.style.opacity -= 0.1;
+      console.log("decrement opacity");
+    }
+    else {
+      clearInterval(fadeEffect);
+      console.log("stop fadeout");
+    }
+  }, interval);
+}
 
 function saveLincoln(interval) {
   if(isNaN(interval)) {
-    interval = 10000
+      interval = 500
+    }
+  fadeOut(interval)
   }
-  $('#foreground').fadeOut(interval);
-}
+
+ button.addEventListener('click', () => saveLincoln(parseInt(interval)));
